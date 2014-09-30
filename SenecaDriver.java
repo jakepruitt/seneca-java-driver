b048493a0f5c298409de5846f62f2ed15517c528
Jake Pruitt <jrpruit1@asu.edu>
2014-09-29 23:37:59 -0700

    Initial commit of SenecaDriver.java

---
+++ SenecaDriver.java
@@ -0,0 +1,34 @@
+/* ==================================================
+= Seneca Java Client                                =
+=                                                   =
+= For any questions about the Seneca or this Java   =
+= driver, check out the docs at                     =
+=  https://github.com/jrpruit1/seneca-java-client   =
+= or feel free to contact me on twitter:            = 
+=  @thejakepruitt                                   =
+==========================================*/
+import java.net.URL;
+import java.net.MalformedURLException;
+
+public class SenecaDriver {
+	private int remotePort;
+	private String remoteHost;
+	private URL remoteURL;
+
+	public SenecaDriver(String remoteHost, int remotePort) {
+		this.remoteHost = remoteHost;
+		this.remotePort = remotePort;
+
+		try {
+			this.remoteURL  = new URL("http", remoteHost, remotePort, null);
+		} catch(MalformedURLException ex) {
+			throw new IllegalArgumentException("Host or Port parameters could not be converted to URL", ex);
+		}
+		
+	}
+
+	public void receiveJSON() {}
+
+	public void sendJSON() {}
+}
+
