5b24e5931fba2899c4203902c1106ebc7735dd6d
Jake Pruitt <jrpruit1@asu.edu>
2014-10-03 01:33:53 -0700

    Added wiremock for mocking servers

--- src/test/java/com/seneca/seneca_java_client/AppTest.java
+++ src/test/java/com/seneca/seneca_java_client/AppTest.java
@@ -1,33 +1,43 @@
 package com.seneca.seneca_java_client;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
-
-
+    
     /**
      * Rigourous Test :-)
      */
     @Test
     public void testApp()
     {
-        SenecaDriver client = new SenecaDriver("facebook.com", 80);
+        SenecaDriver client = new SenecaDriver("localhost", 8080);
         assertTrue( client != null );
     }
+
 
     @Test
     public void testSendJson()
     {
-        SenecaDriver client = new SenecaDriver("facebook", 80);
+        stubFor(get(urlEqualTo("/"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content Type", "text")
+                .withBody("Hello Boston")));
+
+        SenecaDriver client = new SenecaDriver("localhost", 8080);
         SenecaDriver.Message message = client.sendJson(new Object());
+        System.out.println(message);
     }
+
 }
